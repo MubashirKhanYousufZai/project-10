f95f07e6d2e31bc2bdcc8ac712e6dbe624bfcdb2
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-05-03 04:22:25 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+let hostName ="HajiAbdulKarim KhanYousufzai"
+let guestName ="Relatives"
+let main =("Invited you in his grandchilrens NIKKAH ceremony~");
+let invitation =("\nPlease join us on our memorable day. Please pray for us to save us from evill eyes.\n");
+let time =("\n8:00\n");
+let date =("\n8-july-2019\n");
+let venue =("Ayan club");
+let venueAddress=("Block 20 Fedral B Area, Ancholi,Near: Imam bargah Shudah-e-Karbala,KARACHI ")
+let mainMessage =("\nPlease come on time for photography. \n")
+console.log(hostName,guestName,main,invitation,time,date,venue,venueAddress,mainMessage);
+
